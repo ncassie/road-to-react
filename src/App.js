836db0eab44c7fe1e0b5a4a878a7c3d3a404a739
Nicholas Cassie <nicholascassie@pop-os.localdomain>
2022-08-17 06:08:30 -0400

    react component section

--- src/App.js
+++ src/App.js
@@ -19,44 +19,38 @@ const list = [
   },
 ];
 
-function App() {
-  
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <Search />
+    <Search />
     
-      <hr />
-      <List />
-    </div>
-  );
-}
+    <hr />
+    <List />
+  </div>
+);
 
-function List(){
-  return (
-    <ul>
-      {list.map(function (item){
-        return <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      })}
-    </ul>
-  );
-}
 
-function Search(){
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type = "text" />
-    </div>
-  )
-}
+const List = () => (
+  <ul>
+    {list.map((item) => {
+      return <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    })}
+  </ul>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type = "text" />
+  </div>
+)
 
 export default App;
