1bf3000d96017d556985baff661779d164214104
Nicholas Cassie <nicholascassie@pop-os.localdomain>
2022-08-19 11:09:33 -0400

    react side-effects section

--- src/App.js
+++ src/App.js
@@ -27,7 +27,13 @@ const App = () => {
     },
   ];
 
-  let [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
   
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
